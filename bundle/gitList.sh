1138a4aac6b5071212a50b0f4aa88945035a891f
Thomas Belahi <belahi@ipgp.fr>
2014-09-19 16:19:56 +0200

    added a file gitList.sh to keep track of pathogen bundles

---
+++ bundle/gitList.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+cd ~/.vim/bundle
+
+#ctrlp.vim/
+git clone https://github.com/kien/ctrlp.vim.git
+
+#nerdtree/
+git clone https://github.com/scrooloose/nerdtree.git
+
+#powerline/
+git clone https://github.com/Lokaltog/powerline.git
+
+#Pydiction/
+git clone https://github.com/vim-scripts/Pydiction.git
+
+#solarized/
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized solarized
+
+#vim-powerline/
+# deprecated
+
+#vim-scala/
+git clone https://github.com/derekwyatt/vim-scala.git
+
+#fortran.vim
+# already tracked by git
